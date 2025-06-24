d4d553bca7dd550329c4ca4932373e327d0a5515
SamikshaMorya <moryasamiksha52@gmail.com>
2025-06-24 20:19:43 +0530

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
 import session from "express-session";
-import path from "path";
 
 import "./passport/github.auth.js";
 
@@ -17,27 +16,23 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+
+// Optional: Allow frontend to access backend (adjust the URL if needed)
+app.use(cors({
+  origin: "https://git-frontend-two.onrender.com", //  replace with actual frontend URL
+  credentials: true
+}));
 
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
-
-// app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 
 app.listen(PORT, () => {
-	console.log(`Server started on http://localhost:${PORT}`);
-	connectMongoDB();
+  console.log(`Server started on http://localhost:${PORT}`);
+  connectMongoDB();
 });
