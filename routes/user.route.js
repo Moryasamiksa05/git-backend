0b1571dd61ba0a495a2a5896e5162187fb19477a
SamikshaMorya <moryasamiksha52@gmail.com>
2025-06-25 10:15:21 +0530

    user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { getLikes, getUserProfileAndRepos, likeProfile } from "../controllers/user.controller.js";
+import {
+	getLikes,
+	getUserProfileAndRepos,
+	likeProfile
+} from "../controllers/user.controller.js";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
 
 const router = express.Router();
 
+// Public route to get profile & GitHub repos
 router.get("/profile/:username", getUserProfileAndRepos);
+
+// Protected routes (only for logged-in users)
 router.get("/likes", ensureAuthenticated, getLikes);
 router.post("/like/:username", ensureAuthenticated, likeProfile);
 
